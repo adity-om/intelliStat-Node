baf922154c52308fb2ca9100fb532b32bc6260fa
Aditya Om <9410711+adity-om@users.noreply.github.com>
2022-02-06 22:39:03 -0500

    added status handling

--- web_server.cpp
+++ web_server.cpp
@@ -1,8 +1,8 @@
 /*
  * @Author: aditya om 
  * @Date: 2022-02-06 22:15:39 
- * @Last Modified by: aditya om
- * @Last Modified time: 2022-02-06 22:33:52
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2022-02-06 22:38:22
  */
 
 #include "FS.h"
@@ -43,13 +43,28 @@ void handleHome(AsyncWebServerRequest *request){
 }
 
 void handleStatus(AsyncWebServerRequest * request){
-  
+    AsyncResponseStream *response;
+    if(false == requestPreProcess(request, response)){
+        return;
+    }
 
     //Form JSON String to respond to the status AJAX requests from App client
-   
+    String s = "{";
+    s += "\"relay_1_state\":" + String(relay_1_state ? "false":"true") + ",";
+    s += "\"relay_2_state\":" + String(relay_2_state ? "false":"true") + ",";
+    s += "\"relay_3_state\":" + String(relay_3_state ? "false":"true") + ",";
+
+    s += "\"dallas_temperature\":\"" + String(dallas_temperature) + "\",";
+    s += "\"dht_temperature\":\"" + String(dht_temperature) + "\",";
+    s += "\"dht_humidity\":\"" + String(dht_humidity) + "\"";
+    s += "}";
 
     //output to console
-    
-}
+    DBUG("[web_server] '/status' response");
+    DBUGLN(s);
 
+    response->setCode(200);
+    response->print(s);
+    request->send(response);
+}
 
